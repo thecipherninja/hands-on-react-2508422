9dfb1b45ad891e5d797d900ee61d1971a0c2597b
Shimei Sagar Das <68263409+thecipherninja@users.noreply.github.com>
2024-02-12 08:15:48 +0000

    added a navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import ListCast from './components/ListCast';
 import Modals from './components/Modals';
+import Nav from './components/Nav';
 
 function App() {
-  const name = 'StarGazers'
   const [cast, setCast] = useState([]);
   let [memberInfo, setMemberInfo] = useState(null);
 
@@ -17,6 +17,8 @@ function App() {
   });
 
   return (
+    <>
+    <Nav cast={cast} onChoice={(info) => {setMemberInfo(info)}} />
     <div className="container">
       <hgroup>
         <img src="images/group.svg" alt="StarGazers Group" />
@@ -26,6 +28,7 @@ function App() {
         {memberInfo && <Modals member={memberInfo} handleClose={() => { setMemberInfo(null) }} />}
       </hgroup>
     </div>
+    </>
   )
 }
 export default App
